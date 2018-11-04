95b537e2bee7d4bf815276ccf10fae3b0ff78066
Unknown <jakash1997@gmail.com>
2018-11-04 20:43:35 +0530

    Added watching to websites

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 
 My bot to track changes in websites. Deployed at t.me/siteseer_bot .
 
-DM me at linkedin.com/akash-s-joshi with the website you wanna track so I can add you to the bot.
+## Usecases
+
+> For students
+
+* Track exam form website
+* Track student result website
+
+> For devs
+
+* Track pull requests page
+* Update when an issue gets updated
+
+## Commands
+
+/start to get list of commands
+/watch {sitename} to track a website
+/unsub {sitename} to unsubscribe from a website
+/list to get list of websites
+
+DM me at linkedin.com/in/akash-s-joshi to add websites to this list.
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const checksum = (input) => {
 
 bot.sendMessage(admin,`Hello ${people} , the bot just re/started`)
 
-let sites = [{'url':'https://www.goethe.de/ins/in/en/sta/pun/prf/anm.html','chatId':['475757469']},{'url':'http://reg.fiitjee.co','chatId':['603907586']},{'url':'https://jeemain.nic.in/webinfo/Public/Home.aspx','chatId':['603907586']}];
+let sites = [{"url":"https://www.goethe.de/ins/in/en/sta/pun/prf/anm.html","chatId":["475757469"],"checksumString":""},{"url":"http://reg.fiitjee.co","chatId":[],"checksumString":""},{"url":"https://jeemain.nic.in/webinfo/Public/Home.aspx","chatId":[],"checksumString":""}];
 
 let siteList = ['https://www.goethe.de/ins/in/en/sta/pun/prf/anm.html','http://reg.fiitjee.co','https://jeemain.nic.in/webinfo/Public/Home.aspx'];
 
@@ -27,7 +27,11 @@ bot.onText(/\/start/,(msg) =>{
     bot.sendMessage(msg.chat.id,
 		`Welcome to SiteSeer ! 
 		Made by www.linkedin.com/in/akash-s-joshi 👻. 
+
 		/list to list websites
+		/watch {sitename} to watch a site
+		/unsub {sitename} to unsubscribe from the site
+
 		Your chatid is ${msg.chat.id}`)
 })
 
@@ -36,32 +40,35 @@ app.get('/', (req,res) => {
 });
 
 // Matches "/echo [whatever]"
-/*bot.onText(/\/watch (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
+bot.onText(/\/watch (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram 'match' is the result of executing the regexp above on the text content of the message
     const chatId = msg.chat.id;
-    let usermsg = match[1]  
-    usermsg = url.parse(usermsg).hostname
-    console.log(usermsg)
-    let userurl = `http://${usermsg}`
-    console.log(userurl)
-    if(usermsg!=null){          // check whether website is valid
-        
-        userSite[chatId] = userurl
-        sites.push(userurl);
-        sites = sites.filter((v, i, a) => a.indexOf(v) === i);
-        console.log(userSite)
-        console.log(sites)
-        bot.sendMessage(chatId,`Checking ${usermsg} for you !`)
-        if(!cronFlag){
-            job.start();
-            cronFlag = true;
-        }
-    }
-    else bot.sendMessage(chatId,`Could not find the site. Make sure the *http://* part is included in the entered URL`)
-});*/
+	let url = match[1].toLowerCase()  
+    url = /^http(s)?:\/\//.test(url) ? url : `http://${url}`;
+    if(siteList.indexOf(url) == -1){
+		siteList.push(url);
+		sites.push({url:url,chatId:[],checksumString:""})
+	}
+		
+	sites.forEach((element)=>{
+		if(element.url == url){
+			let flag = true;
+			element.chatId.forEach((element)=>{
+				if(element == msg.chat.id){
+					bot.sendMessage(msg.chat.id,`Already subscribed`)
+					flag = false
+					return true
+				}
+			})
+			if(flag){
+				element.chatId.push(msg.chat.id)
+				bot.sendMessage(msg.chat.id,`Checking ${url} for you !`)
+				return true;
+			}
+			else return true;
+		}
+	})
+});
 
 bot.onText(/\/list/,(msg)=>{
 	let temp = 'Sites currently being checked are \n'
@@ -75,34 +82,10 @@ bot.onText(/\/list/,(msg)=>{
 	bot.sendMessage(msg.chat.id,temp)
 })
 
-bot.onText(/\/subscribe (.+)/,(msg,match)=>{
-	if(siteList.indexOf(match[1])!=-1){
-		sites.forEach((element)=>{
-			if(element.url==match[1]){
-				let flag = true;
-				element.chatId.forEach((element)=>{
-					if(element==msg.chat.id){
-						bot.sendMessage(msg.chat.id,`Already subscribed`)
-						flag = false
-						return true
-					}
-				})
-				if(flag){
-					element.chatId.push(msg.chat.id)
-					bot.sendMessage(msg.chat.id,`Successfully subscribed to ${match[1]}`)
-					return true;
-				}
-				else return true;
-			}
-		})
-	}
-	else bot.sendMessage(msg.chat.id,`${match[1]} isn't a valid site. Please check /list for available websites`)
-})
-
 bot.onText(/\/unsub (.+)/,(msg,match)=>{
 	if(siteList.indexOf(match[1])!=-1){
 		sites.forEach((element)=>{
-			if(element.url==match[1]){
+			if(element.url == match[1]){
 				element.chatId = element.chatId.filter((value)=>{
 					return value != msg.chat.id;
 				})
